159ef3308fc802941341650948ccbd119a84f2de
khoalm4 <khoalm4@vng.com.vn>
2022-03-27 12:21:14 +0700

    save final cd map

--- instance_segment_s2looking_dependent_model.py
+++ instance_segment_s2looking_dependent_model.py
@@ -15,7 +15,9 @@ from utils.hparams import HParam
 from utils.images import *
 from utils.hungarian import *
 
-def main(hp, mode, weights, trained_path, saved_path, threshold=0.5, batch_size=8, save_sub_mask=False):
+
+def main(hp, mode, weights, trained_path, saved_path, threshold=0.5, batch_size=8, save_sub_mask=False,
+         cm_weights=[0.5, 0.5]):
     assert (0 <= mode < 3)
     assert os.path.isfile(trained_path)
 
@@ -24,7 +26,7 @@ def main(hp, mode, weights, trained_path, saved_path, threshold=0.5, batch_size=
 
     cd_save_path = os.path.join(saved_path, "cd")
     hungarian_cd_save_path = os.path.join(saved_path, "hungarian_cd")
-    masks_save_path = os.path.join(saved_path, "masks")
+    final_cd_path = os.path.join(saved_path, "final_cd")
 
     if not os.path.exists(saved_path):
         os.makedirs(saved_path)
@@ -41,9 +43,9 @@ def main(hp, mode, weights, trained_path, saved_path, threshold=0.5, batch_size=
     if not os.path.exists(hungarian_cd_save_path):
         os.makedirs(hungarian_cd_save_path)
 
-    if not os.path.exists(masks_save_path):
-        os.makedirs(masks_save_path)
-        
+    if not os.path.exists(final_cd_path):
+        os.makedirs(final_cd_path)
+
     model = DependentResUnetMultiDecoder().cuda()
     checkpoint = torch.load(trained_path)
     model.load_state_dict(checkpoint["state_dict"])
@@ -89,30 +91,50 @@ def main(hp, mode, weights, trained_path, saved_path, threshold=0.5, batch_size=
             cd_i2 = data['y'].cuda()
             outputs = model(cd_i1, cd_i2)
 
-            cm = (outputs['cm'].cpu().numpy() >= threshold) * 1
-            x = (outputs['x'].cpu().numpy() >= threshold) * 1
-            y = (outputs['y'].cpu().numpy() >= threshold) * 1
+            cm_prob = outputs['cm'].cpu().numpy()
+            x_prob = outputs['x'].cpu().numpy()
+            y_prob = outputs['y'].cpu().numpy()
+
+            cm = (cm_prob >= threshold) * 1
+            x = (x_prob >= threshold) * 1
+            y = (y_prob >= threshold) * 1
 
             for i in range(cm.shape[0]):
                 filename = dataset.files[idx * batch_size + i]
                 filename = os.path.basename(filename['image1'])[:-4]
 
-                masks1 = save_mask_and_contour(x[i, 0, ...], x[i, 1, ...], NUCLEI_PALETTE, os.path.join(img1_save_path, "is_{filename}.png".format(filename=filename))).astype(int)
-                masks2 = save_mask_and_contour(y[i, 0, ...], y[i, 1, ...], NUCLEI_PALETTE, os.path.join(img2_save_path, "is_{filename}.png".format(filename=filename))).astype(int)
+                masks1 = save_mask_and_contour(x[i, 0, ...], x[i, 1, ...], NUCLEI_PALETTE, os.path.join(img1_save_path,
+                                                                                                        "is_{filename}.png".format(
+                                                                                                            filename=filename))).astype(
+                    int)
+                masks2 = save_mask_and_contour(y[i, 0, ...], y[i, 1, ...], NUCLEI_PALETTE, os.path.join(img2_save_path,
+                                                                                                        "is_{filename}.png".format(
+                                                                                                            filename=filename))).astype(
+                    int)
 
                 masks1 = torch.from_numpy(masks1)
                 masks2 = torch.from_numpy(masks2)
                 # Hungarian algorithm
-                cd_map = change_detection_map(masks1, masks2, 256, 256)
-                cd_image = (cd_map * 255).astype(np.uint8)
-                # im = Image.fromarray(cd_image, mode='P')
-                # im.save(os.path.join(cd_save_path, "{filename}.png".format(filename=filename)))
+                hungarian_cd_map = change_detection_map(masks1, masks2, 256, 256)
+                hungarian_cd_image = (hungarian_cd_map * 255).astype(np.uint8)
+
                 mask_color_1 = convert_to_color_map(masks1)
                 mask_color_2 = convert_to_color_map(masks2)
-                plot_and_save(mask_color_1, mask_color_2, cd_image, os.path.join(hungarian_cd_save_path, "{filename}.png".format(filename=filename)))
+                plot_and_save(mask_color_1, mask_color_2, hungarian_cd_image,
+                              os.path.join(hungarian_cd_save_path, "{filename}.png".format(filename=filename)))
 
-                cm_im = Image.fromarray((cm[i, 0, ...]*255).astype(np.uint8), mode='P')
+                cm_im = Image.fromarray((cm[i, 0, ...] * 255).astype(np.uint8), mode='P')
                 cm_im.save(os.path.join(cd_save_path, "{filename}.png".format(filename=filename)))
+
+                # Calculate final map
+                cm_x_prob = np.multiply(x_prob[i, 0, ...], hungarian_cd_map)
+                cm_y_prob = np.multiply(y_prob[i, 0, ...], hungarian_cd_map)
+
+                final_cm_map = np.max(cm_x_prob, cm_y_prob)
+                final_cm_map = final_cm_map * cm_weights[0] + cm_prob[i, 0, ...] * cm_weights[1]
+                final_cm_map = (final_cm_map >= threshold) * 255
+                final_cm_map = Image.fromarray(final_cm_map.astype(np.uint8), mode='P')
+                final_cm_map.save(os.path.join(final_cd_path, "{filename}.png".format(filename=filename)))
 
     print("Validation Loss: {:.4f} Acc: {:.4f}".format(valid_loss.avg, valid_acc.avg))
 
@@ -138,4 +160,3 @@ if __name__ == '__main__':
 
     hp = HParam(args.config)
     main(hp, int(args.mode), weights, args.pretrain, args.savepath, args.threshold, args.batchsize)
-
