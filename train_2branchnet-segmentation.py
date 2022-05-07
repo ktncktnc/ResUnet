b211e815e96b81b89639226f247c4f1d0b4184f9
khoalm4 <khoalm4@vng.com.vn>
2022-05-07 17:30:31 +0700

    update train segmentation model

---
+++ train_2branchnet-segmentation.py
@@ -0,0 +1,224 @@
+import warnings
+from torch.utils.data import DataLoader
+from torchvision import models
+from tqdm import tqdm
+from utils.hparams import HParam
+from dataset.alabama_segment import AlabamaDataset
+from utils import metrics
+from core.mixedmodel_cd_based import DependentResUnetMultiDecoder
+from utils.logger import MyWriter
+import torch
+import argparse
+import os
+import ssl
+warnings.simplefilter("ignore", UserWarning)
+warnings.simplefilter("ignore", FutureWarning)
+
+
+def main(hpconfig, num_epochs, resume, name, device, training_weight=None):
+    ssl._create_default_https_context = ssl._create_unverified_context
+
+    checkpoint_dir = "{}/{}".format(hpconfig.checkpoints, name)
+    os.makedirs(checkpoint_dir, exist_ok=True)
+
+    os.makedirs("{}/{}".format(hpconfig.log, name), exist_ok=True)
+    writer = MyWriter("{}/{}".format(hpconfig.log, name))
+
+    # Model
+    resnet = models.resnet34(pretrained=True)
+    model = DependentResUnetMultiDecoder(resnet=resnet).to(device)
+
+    # set up binary cross entropy and dice loss
+    criterion = metrics.BCEDiceLoss(weight=[0.1, 0.9])
+
+    # Optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=hpconfig.lr)
+
+    # decay LR
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.1)
+
+    step = 0
+    # starting params
+    s_best_loss = 999
+    start_epoch = 0
+    # optionally resume from a checkpoint
+    if resume:
+        if os.path.isfile(resume):
+            print("=> loading checkpoint '{}'".format(resume))
+            checkpoint = torch.load(resume, map_location=device)
+
+            start_epoch = checkpoint["epoch"]
+
+            s_best_loss = checkpoint["s_best_loss"]
+
+            step = checkpoint["step"]
+
+            model.load_state_dict(checkpoint["state_dict"])
+            optimizer.load_state_dict(checkpoint["optimizer"])
+            print(
+                "=> loaded checkpoint '{}' (epoch {})".format(
+                    resume, checkpoint["epoch"]
+                )
+            )
+        else:
+            print("=> no checkpoint found at '{}'".format(resume))
+
+    # get data
+    # cd
+    cd_dataset_train = AlabamaDataset(hpconfig.cd_dset_dir, "train")
+    cd_dataset_val = AlabamaDataset(hpconfig.cd_dset_dir, "test")
+    # cd_dataset_test = AlabamaDataset(hpconfig.cd_dset_dir, "test")
+
+    cd_train_dataloader = DataLoader(
+        cd_dataset_train, batch_size=hpconfig.batch_size, num_workers=2, shuffle=True
+    )
+    cd_val_dataloader = DataLoader(
+        cd_dataset_val, batch_size=hpconfig.batch_size, num_workers=2, shuffle=False
+    )
+
+    for epoch in range(start_epoch, num_epochs):
+        print("Epoch {}/{}".format(epoch, num_epochs - 1))
+        print("-" * 10)
+
+        # step the learning rate scheduler
+        lr_scheduler.step()
+
+        # run training and validation
+        # logging accuracy and loss
+        s_train_acc = metrics.MetricTracker()
+        s_train_loss = metrics.MetricTracker()
+
+        loader = tqdm(cd_train_dataloader, desc="Training")
+        # iterate over data
+        for data in loader:
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            images = data['image'].to(device)
+            labels = data['mask'].to(device)
+
+            outputs = model.segment_forward(images)
+
+
+            # Segment loss
+            loss = 0.0
+            for i in range(2):
+                _loss = criterion(outputs[:, i, ...], labels[:, i, ...])
+                loss += training_weight[i] * _loss
+
+            # cd_train_acc.update(metrics.dice_coeff(outputs['cm'], cd_labels), outputs['cm'].size(0))
+            # cd_train_loss.update(cd_loss.data.item(), outputs['cm'].size(0))
+
+            s_train_acc.update(metrics.dice_coeff(outputs, labels), outputs.size(0))
+            s_train_loss.update(loss.data.item(), outputs.size(0))
+            # backward
+            loss.backward()
+            optimizer.step()
+
+            # tensorboard logging
+            if (step + 1) % hpconfig.logging_step == 0:
+                writer.log_training(s_train_loss.avg, s_train_acc.avg, step, "s_training")
+                # writer.log_training(cd_train_loss.avg, cd_train_acc.avg, step, "cd_training")
+                loader.set_description(
+                    "S training Loss: {:.4f} dice: {:.4f}".format(
+                        s_train_loss.avg, s_train_acc.avg
+                    )
+                )
+
+            # Validation
+            if (step + 1) % hpconfig.validation_interval == 0:
+                valid_metrics = validation(
+                    cd_val_dataloader, model, criterion, device, training_weight, True, writer, step
+                )
+                save_path = os.path.join(
+                    checkpoint_dir, "%s_checkpoint_%04d.pt" % (name, step)
+                )
+                # store best loss and save a model checkpoint
+                s_best_loss = min(valid_metrics["s_valid_loss"], s_best_loss)
+                torch.save(
+                    {
+                        "step": step,
+                        "epoch": epoch,
+                        "arch": "ResUnet",
+                        "state_dict": model.state_dict(),
+                        "s_best_loss": s_best_loss,
+                        "optimizer": optimizer.state_dict(),
+                    },
+                    save_path,
+                )
+                print("Saved checkpoint to: %s" % save_path)
+
+            step += 1
+
+
+def validation(cd_valid_loader, model, criterion, device, training_weight, write_log=False, logger=None, step=None):
+    print("\nValidation...")
+    # logging accuracy and loss
+    s_valid_acc = metrics.MetricTracker()
+    s_valid_loss = metrics.MetricTracker()
+
+    # switch to evaluate mode
+    model.eval()
+
+    # Iterate over data.
+    for data in cd_valid_loader:
+        # get the inputs and wrap in Variable
+        images = data['image'].to(device)
+        labels = data['mask'].to(device)
+
+        outputs = model.segment_forward()
+
+        loss = 0.0
+        for i in range(2):
+            _loss = criterion(outputs[:, i, ...], labels[:, i, ...])
+            loss += training_weight[i] * _loss
+
+        s_valid_acc.update(metrics.dice_coeff(outputs, labels), outputs.size(0))
+        s_valid_loss.update(loss.data.item(), outputs.size(0))
+
+    if write_log:
+        logger.log_validation(s_valid_acc.avg, s_valid_loss.avg, step, "s_validation")
+
+        print("Segment validation loss: {:.4f} Acc: {:.4f}"
+          .format(s_valid_loss.avg, s_valid_acc.avg))
+
+    model.train()
+
+    return {
+        "s_valid_loss": s_valid_loss.avg,
+        "s_valid_acc": s_valid_acc.avg,
+    }
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Road and Building Extraction")
+    parser.add_argument(
+        "-c", "--config", type=str, required=True, help="yaml file for configuration"
+    )
+    parser.add_argument(
+        "--epochs",
+        default=75,
+        type=int,
+        metavar="N",
+        help="number of total epochs to run",
+    )
+    parser.add_argument(
+        "--resume",
+        default="",
+        type=str,
+        metavar="PATH",
+        help="path to latest checkpoint (default: none)",
+    )
+    parser.add_argument("--name", default="default", type=str, help="Experiment name")
+    parser.add_argument("--mode", default=0, type=int, help="Training mode")
+    parser.add_argument("--device", default="cuda:0", type=str, help="Device ID")
+    args = parser.parse_args()
+
+    hp = HParam(args.config)
+    with open(args.config, "r") as f:
+        hp_str = "".join(f.readlines())
+
+    weights = [1.0, 0.1]
+    device = torch.device(args.device)
+
+    main(hp, num_epochs=args.epochs, resume=args.resume, name=args.name, device=device, training_weight=weights)
