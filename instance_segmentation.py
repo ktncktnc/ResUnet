03453d90c393beb5c0ccb943f26c5b7055308835
khoalm4 <khoalm4@vng.com.vn>
2022-06-23 00:32:50 +0700

    np savetxt -> save

--- instance_segmentation.py
+++ instance_segmentation.py
@@ -161,8 +161,8 @@ def main(hp, mode, split, trained_path, saved_path, threshold=0.5, batch_size=8,
                     full_x_probs = cv2.resize(full_x_probs, (dataset.width, dataset.height), interpolation=cv2.INTER_LINEAR)
                     full_y_probs = cv2.resize(full_y_probs, (dataset.width, dataset.height), interpolation=cv2.INTER_LINEAR)
 
-                    np.savetxt(os.path.join(prob_img1_save_path, "prob_{filename}.txt".format(filename=filename)), full_x_probs)
-                    np.savetxt(os.path.join(prob_img2_save_path, "prob_{filename}.txt".format(filename=filename)), full_y_probs)
+                    np.save(os.path.join(prob_img1_save_path, "prob_{filename}.txt".format(filename=filename)), full_x_probs)
+                    np.save(os.path.join(prob_img2_save_path, "prob_{filename}.txt".format(filename=filename)), full_y_probs)
                     # Hungarian algorithm
                     if cd:
                         masks1 = torch.from_numpy(masks1)
