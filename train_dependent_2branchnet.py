655fdfbfdf580de1b3e2bdae01edfe86e793de1a
khoalm4 <khoalm4@vng.com.vn>
2022-06-24 22:06:48 +0700

    update siamese model feature size

--- train_dependent_2branchnet.py
+++ train_dependent_2branchnet.py
@@ -183,12 +183,11 @@ def main(hpconfig, num_epochs, resume, segmentation_weights, name, device, train
 
             step += 1
 
-        if epoch % 2 == 0:
-            print("Test...")
-            test_metrics = validation(
-                cd_test_dataloader, model, criterion, device, training_weight, validation_metrics
-            )
-            print(test_metrics)
+        print("Test...")
+        test_metrics = validation(
+            cd_test_dataloader, model, criterion, device, training_weight, validation_metrics
+        )
+        print(test_metrics)
 
 
 def validation(
