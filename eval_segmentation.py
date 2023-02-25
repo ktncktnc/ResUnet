005731dec64c48e55c5eca3da0beff34f9ecd549
ktncktnc <doremon6a1@gmail.com>
2023-02-26 01:42:16 +0700

    Refactor

---
+++ eval_segmentation.py
@@ -0,0 +1,266 @@
+import os
+import cv2
+import torch
+import argparse
+import numpy as np
+import pandas as pd
+import albumentations as albums
+import torchmetrics
+from albumentations.pytorch import ToTensorV2
+from core.siamese_resunet_cd_segmenation_based import SiameseResUnetSegmentationBased
+from utils import metrics
+from dataset.s2looking_allmask import S2LookingAllMask
+from dataset.xbd import XView2Dataset
+from dataset.utils import *
+from PIL import Image, ImagePalette
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from utils.hparams import HParam
+from utils.images import *
+from utils.hungarian import *
+
+
+def main(hp, mode, split, trained_path, saved_path, threshold=0.5, batch_size=8,
+         cm_weights=None, device=torch.device("cuda"), dataset_split=2, cd=True):
+    if cm_weights is None:
+        cm_weights = [0.3, 0.7]
+    assert (0 <= mode < 3)
+    assert os.path.isfile(trained_path)
+
+    img1_save_path = os.path.join(saved_path, "img1")
+    img2_save_path = os.path.join(saved_path, "img2")
+
+    prob_img1_save_path = os.path.join(saved_path, "prob_img1")
+    prob_img2_save_path = os.path.join(saved_path, "prob_img2")
+
+    cd_save_path = os.path.join(saved_path, "cd")
+    hungarian_cd_save_path = os.path.join(saved_path, "hungarian_cd")
+    final_cd_path = os.path.join(saved_path, "final_cd")
+
+    if not os.path.exists(saved_path):
+        os.makedirs(saved_path)
+
+    if not os.path.exists(img1_save_path):
+        os.makedirs(img1_save_path)
+
+    if not os.path.exists(img2_save_path):
+        os.makedirs(img2_save_path)
+
+    if not os.path.exists(cd_save_path):
+        os.makedirs(cd_save_path)
+
+    if not os.path.exists(prob_img1_save_path):
+        os.makedirs(prob_img1_save_path)
+
+    if not os.path.exists(prob_img2_save_path):
+        os.makedirs(prob_img2_save_path)
+
+    if not os.path.exists(hungarian_cd_save_path):
+        os.makedirs(hungarian_cd_save_path)
+
+    if not os.path.exists(final_cd_path):
+        os.makedirs(final_cd_path)
+
+    model = SiameseResUnetSegmentationBased().to(device)
+    checkpoint = torch.load(trained_path, map_location=device)
+    model.load_segmentation_weight(checkpoint["state_dict"])
+    model.eval()
+
+    training_metrics = torchmetrics.MetricCollection(
+        {
+            "Dice": torchmetrics.Dice(average='none', num_classes=2, mdmc_reduce='global'),
+            "Precision": torchmetrics.Precision(average='none', num_classes=2, mdmc_reduce='global'),
+            "Recall": torchmetrics.Recall(average='none', num_classes=2, mdmc_reduce='global')
+        },
+        prefix='test_'
+    )
+
+    transform = albums.Compose([
+        albums.Resize(256, 256),
+        albums.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+        ToTensorV2(),
+    ],
+        additional_targets={
+            'image0': 'image',
+            'mask1': 'mask',
+            'mask2': 'mask',
+            'border_mask1': 'mask',
+            'border_mask2': 'mask'
+        }
+    )
+
+    dataset = S2LookingAllMask(hp.cd_dset_dir, split, transform, dataset_split, without_mask=False)
+    # dataset = XView2Dataset(root_dir=hp.cd_dset_dir, mode=split, with_prob=False)
+    dataloader = DataLoader(
+        dataset, batch_size=batch_size, num_workers=2, shuffle=False
+    )
+
+    cd_branch_acc = metrics.MetricTracker()
+    hungarian_branch_acc = metrics.MetricTracker()
+    final_acc = metrics.MetricTracker()
+
+    criterion = metrics.BCEDiceLoss(weight=[0.1, 0.9])
+
+    loader = tqdm(dataloader, desc="Evaluating")
+
+    nuclei_palette = ImagePalette.random()
+
+    img_height, img_width = dataset.get_full_resized_shape()
+
+    full_cm = np.zeros((img_height, img_width))
+    full_label = np.zeros((img_height, img_width))
+    full_x = np.zeros((2, img_height, img_width))
+    full_y = np.zeros((2, img_height, img_width))
+    full_x_probs = np.zeros((img_height, img_width), dtype=np.float64)
+    full_y_probs = np.zeros((img_height, img_width), dtype=np.float64)
+    full_cm_probs = np.zeros((img_height, img_width), dtype=np.float64)
+
+    with torch.no_grad():
+        for (idx, data) in enumerate(loader):
+            cd_i1 = data['x'].to(device)
+            cd_i2 = data['y'].to(device)
+            #cd_labels = data['mask'].cuda()
+            outputs_i1 = model.segment_forward(cd_i1, domain_classify=False)
+            outputs_i2 = model.segment_forward(cd_i2, domain_classify=False)
+
+            #cd_branch_acc.update(metrics.dice_coeff(outputs['cm'], cd_labels), outputs['cm'].size(0))
+
+            #cm_probs = outputs['cm'].cpu().numpy()
+            x_probs = outputs_i1.cpu().numpy()
+            y_probs = outputs_i2.cpu().numpy()
+
+            #cm = (cm_probs >= threshold) * 1
+            x = (x_probs >= threshold) * 1
+            y = (y_probs >= threshold) * 1
+
+            hg_probs = []
+            final_probs = []
+
+            for i in range(x.shape[0]):
+                # Get file name
+                files = dataset.files[idx * batch_size + i]
+                print(files)
+                divide = files['divide']
+                x1, x2, y1, y2 = dataset.get_resized_coord(divide)
+                # S2Looking: filename = os.path.basename(files['image1'])[:-4]
+                pre_name = os.path.basename(files['pre_img'])[:-4]
+                post_name = os.path.basename(files['post_img'])[:-4]
+
+                full_x[:, x1:x2, y1:y2] = x[i, :, ...]
+                full_y[:, x1:x2, y1:y2] = y[i, :, ...]
+
+                full_x_probs[x1:x2, y1:y2] = x_probs[i, 0, ...]
+                full_y_probs[x1:x2, y1:y2] = y_probs[i, 0, ...]
+
+                if divide >= dataset.divide*dataset.divide - 1:
+                    # Colorize instance segmentation map and save
+                    masks1 = save_mask_and_contour(
+                        full_x[0, ...], full_x[1, ...], nuclei_palette,
+                        os.path.join(img1_save_path, "{filename}.png".format(filename=pre_name)),
+                        (dataset.width, dataset.height)
+                    ).astype(int)
+
+                    masks2 = save_mask_and_contour(
+                        full_y[0, ...], full_y[1, ...], nuclei_palette,
+                        os.path.join(img2_save_path, "{filename}.png".format(filename=post_name)),
+                        (dataset.width, dataset.height)
+                    ).astype(int)
+
+                    full_x_probs = cv2.resize(full_x_probs, (dataset.width, dataset.height), interpolation=cv2.INTER_LINEAR)
+                    full_y_probs = cv2.resize(full_y_probs, (dataset.width, dataset.height), interpolation=cv2.INTER_LINEAR)
+
+                    # hdf_x_probs = pd.HDFStore(os.path.join(prob_img1_save_path, "{filename}.hdf".format(filename=pre_name)))
+                    # hdf_y_probs = pd.HDFStore(os.path.join(prob_img2_save_path, "{filename}.hdf".format(filename=post_name)))
+                    #
+                    # hdf_x_probs.append("a", pd.DataFrame(full_x_probs))
+                    # hdf_y_probs.append("a", pd.DataFrame(full_y_probs))
+                    #
+                    # hdf_x_probs.close()
+                    # hdf_y_probs.close()
+
+                    np.savez_compressed(os.path.join(prob_img1_save_path, "{filename}.npz".format(filename=pre_name)), a=full_x_probs)
+                    np.savez_compressed(os.path.join(prob_img2_save_path, "{filename}.npz".format(filename=post_name)), a=full_y_probs)
+                    # Hungarian algorithm
+                    if cd:
+                        masks1 = torch.from_numpy(masks1)
+                        masks2 = torch.from_numpy(masks2)
+
+                        hg_map = change_detection_map(masks1, masks2, dataset.height, dataset.width)
+                        gt_cd = (np.array(Image.open(files["mask"])) / 255.0).astype('int')
+
+                        training_metrics(
+                            target=torch.from_numpy(gt_cd[np.newaxis, :, :]),
+                            preds=torch.from_numpy(hg_map[np.newaxis, :, :])
+                        )
+                        hungarian_branch_acc.update(metrics.np_dice_coeff(hg_map[np.newaxis, :, :], gt_cd[np.newaxis, :, :]), 1)
+
+                        hg_img = (hg_map * 255).astype(np.uint8)
+
+                        # mask_color_1 = convert_to_color_map(masks1, dataset.width, dataset.height)
+                        # mask_color_2 = convert_to_color_map(masks2, dataset.width, dataset.height)
+                        cv2.imwrite(os.path.join(hungarian_cd_save_path, "cd_{filename}.png".format(filename=post_name)),
+                                    hg_img)
+                        # plot_and_save(mask_color_1, mask_color_2, hg_img,
+                        #               os.path.join(hungarian_cd_save_path, "{filename}.png".format(filename=filename)))
+            #
+            #         # Save CM from CD branch
+            #         #cm_im = Image.fromarray((full_cm * 255).astype(np.uint8), mode='P')
+            #         #cm_im.save(os.path.join(cd_save_path, "cd_{filename}.png".format(filename=filename)))
+            #
+            #         # Calculate final CM
+            #         #cm_x_probs = np.multiply(full_x_probs, hg_map)
+            #         #cm_y_probs = np.multiply(full_y_probs, hg_map)
+            #
+            #         hg_prob = np.maximum(cm_x_probs, cm_y_probs)
+            #         hg_probs.append(hg_prob)
+            #
+            #         # final_prob = hg_prob * cm_weights[0] + cm_probs[i, 0, ...] * cm_weights[1]
+            #         final_prob = np.maximum(hg_prob, full_cm_probs)
+            #         final_probs.append(final_prob)
+            #
+            #         final_map = (final_prob >= threshold) * 255
+            #         final_map = Image.fromarray(final_map.astype(np.uint8), mode='P')
+            #         final_map.save(os.path.join(final_cd_path, "final_{filename}.png".format(filename=filename)))
+            #
+            # hg_probs = np.array(hg_probs)
+            # final_probs = np.array(final_probs)
+
+            #hungarian_branch_acc.update(metrics.np_dice_coeff((hg_probs >= threshold)*1, cd_labels.cpu().numpy()), hg_probs.shape[0])
+            #final_acc.update(metrics.np_dice_coeff((final_probs >= threshold)*1, cd_labels.cpu().numpy()), final_probs.shape[0])
+
+    values = training_metrics.compute()
+    print(values)
+    print("CD Branch dice: {:.4f} Hg dice: {:.4f} Final dice: {:.4f}"
+          .format(cd_branch_acc.avg, hungarian_branch_acc.avg, final_acc.avg))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Validate ResUnet")
+    parser.add_argument("--mode", default=1)
+
+    parser.add_argument('--cd', action='store_true')
+    parser.add_argument('--no-cd', dest='cd', action='store_false')
+    parser.set_defaults(cd=True)
+
+    parser.add_argument("--pretrain", type=str)
+    parser.add_argument("--savepath", type=str)
+    parser.add_argument("--threshold", default=0.5, type=float)
+    parser.add_argument("--batchsize", default=8, type=int)
+    parser.add_argument("--dset_divide", default=2, type=int)
+    parser.add_argument(
+        "-c", "--config", type=str, required=True, help="yaml file for configuration"
+    )
+    parser.add_argument("--split", default="val", type=str)
+    parser.add_argument("--device", default="cuda:0", type=str, help="Device ID")
+
+
+    args = parser.parse_args()
+
+    weights = [1.0]
+    if int(args.mode) == 1:
+        weights = [1.0, 0.1]
+    elif int(args.mode) == 2:
+        weights = [1.0, 0.1, 0.05]
+    device = torch.device(args.device)
+    hp = HParam(args.config)
+    main(hp, int(args.mode), args.split, args.pretrain, args.savepath, args.threshold, args.batchsize, [0.3, 0.7], device, args.dset_divide, args.cd)
---
+++ train_segmentation_baseline.py
@@ -0,0 +1,326 @@
+import warnings
+
+import torchmetrics
+from torch.utils.tensorboard import SummaryWriter
+from torch.utils.data import DataLoader
+from torchvision import models
+from tqdm import tqdm
+from utils.hparams import HParam
+import numpy as np
+from dataset.alabama_segment import AlabamaDataset
+from dataset.s2looking_randomcrop import S2LookingRandomCrop
+from dataset.s2looking_allmask import S2LookingAllMask
+from dataset.xbd import XView2Dataset
+from utils import metrics
+from core.mixedmodel_cd_based import DependentResUnetMultiDecoder
+from utils.metrics import Dice, TrackingMetric
+import torch
+import argparse
+import os
+import ssl
+
+warnings.simplefilter("ignore", UserWarning)
+warnings.simplefilter("ignore", FutureWarning)
+
+
+def main(hpconfig, num_epochs, resume, name, device, training_weight=None):
+    ssl._create_default_https_context = ssl._create_unverified_context
+
+    writer = SummaryWriter('runs/{name}'.format(name=name))
+
+    domain_loss_weight = 0.05
+    max_domain_loss_weight = 0.6
+    domain_loss_weight_decay = 1.25
+
+    checkpoint_dir = "{}/{}".format(hpconfig.checkpoints, name)
+    os.makedirs(checkpoint_dir, exist_ok=True)
+
+    os.makedirs("{}/{}".format(hpconfig.log, name), exist_ok=True)
+    # writer = MyWriter("{}/{}".format(hpconfig.log, name))
+
+    # Model
+    resnet = models.resnet50(pretrained=True)
+    model = DependentResUnetMultiDecoder(resnet=resnet).to(device)
+
+    # set up binary cross entropy and dice loss
+    criterion = metrics.BCEDiceLoss(weight=[0.1, 0.9])
+
+    # Optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=hpconfig.lr)
+
+    # decay LR
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
+
+    training_metrics = torchmetrics.MetricCollection(
+        {
+            "Loss": TrackingMetric(name="loss"),
+            "Segmentation_Loss": TrackingMetric(name="segmentation_loss"),
+            #"Domain_Loss": TrackingMetric(name="domain_loss"),
+            "Segmentation_Dice": Dice(),
+            #"Domain_Accuracy": torchmetrics.classification.Accuracy(),
+        },
+        prefix='train_'
+    )
+    validation_metrics = training_metrics.clone(prefix="validation_")
+
+    step = 0
+    # starting params
+    s_best_loss = 999
+    start_epoch = 0
+    # optionally resume from a checkpoint
+    if resume:
+        if os.path.isfile(resume):
+            print("=> loading checkpoint '{}'".format(resume))
+            checkpoint = torch.load(resume, map_location=device)
+
+            start_epoch = checkpoint["epoch"]
+            s_best_loss = checkpoint["s_best_loss"]
+            step = checkpoint["step"]
+
+            model.load_segmentation_weight(checkpoint["state_dict"])
+            optimizer.load_state_dict(checkpoint["optimizer"])
+            print(
+                "=> loaded checkpoint '{}' (epoch {})".format(
+                    resume, checkpoint["epoch"]
+                )
+            )
+        else:
+            print("=> no checkpoint found at '{}'".format(resume))
+
+    # get data
+    # cd
+
+    alb_dataset_train = AlabamaDataset(hpconfig.segment_dset_dir, "train")
+    alb_dataset_val = AlabamaDataset(hpconfig.segment_dset_dir, "test")
+    # s2l_dataset_train = S2LookingRandomCrop(hpconfig.dset2_dir, "train_cr")
+    # s2l_dataset_val = S2LookingAllMask(hpconfig.dset2_dir, "test")
+    # s2l_dataset_train = XView2Dataset(hpconfig.dset2_dir, mode='train', with_prob=False, is_cr=True)
+    # s2l_dataset_val = XView2Dataset(hpconfig.dset2_dir, mode="test", with_prob=False, is_cr=True)
+
+    alb_train_dataloader = DataLoader(
+        alb_dataset_train, batch_size=hpconfig.batch_size, num_workers=2, shuffle=True
+    )
+    alb_val_dataloader = DataLoader(
+        alb_dataset_val, batch_size=hpconfig.batch_size, num_workers=2, shuffle=False
+    )
+
+    # s2l_train_dataloader = DataLoader(
+    #     s2l_dataset_train, batch_size=int(hpconfig.batch_size / 2), num_workers=2, shuffle=True
+    # )
+    # s2l_val_dataloader = DataLoader(
+    #     s2l_dataset_val, batch_size=int(hpconfig.batch_size / 2), num_workers=2, shuffle=False
+    # )
+
+    loader_len = len(alb_dataset_train)
+    nllloss = torch.nn.NLLLoss().to(device)
+
+    for epoch in range(start_epoch, num_epochs):
+        alb_train_batch = iter(alb_train_dataloader)
+        # s2l_train_batch = iter(s2l_train_dataloader)
+
+        start_steps = epoch * loader_len
+        total_steps = num_epochs * loader_len
+
+        print("Epoch {}/{}".format(epoch, num_epochs - 1))
+        print("-" * 10)
+
+        # step the learning rate scheduler
+        lr_scheduler.step()
+
+        loader = tqdm(alb_train_dataloader, desc="segment training")
+        # iterate over data
+        for (i, s_data) in enumerate(loader):
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            p = float(i + start_steps) / total_steps
+            alpha = 2. / (1. + np.exp(-10 * p)) - 1
+
+            # s_data = next(alb_train_batch)
+            s_images = s_data['image'].to(device)
+            s_groundtruth = s_data['mask'].to(device)
+            # s_domains = torch.zeros(s_images.shape[0]).long().to(device)
+
+            # t_data = next(s2l_train_batch)
+            # t_image1 = t_data['x'].to(device)
+            # t_image2 = t_data['y'].to(device)
+            # t_domains = torch.ones(t_image1.shape[0] + t_image2.shape[0]).long().to(device)
+
+            s_outputs, s_output_domains = model.segment_forward(s_images, alpha=alpha)
+            # t_output_domains1 = model.domain_classify(x=t_image1, alpha=alpha)
+            # t_output_domains2 = model.domain_classify(x=t_image2, alpha=alpha)
+            # t_output_domains = torch.cat((t_output_domains1, t_output_domains2), 0)
+
+            # domains = torch.cat((s_domains, t_domains), dim=0).cpu()
+            # output_domains = torch.cat((s_output_domains, t_output_domains), 0).cpu()
+
+            # Segment loss
+            segment_loss = 0.0
+            for i in range(2):
+                _loss = criterion(s_outputs[:, i, ...], s_groundtruth[:, i, ...])
+                segment_loss += training_weight[i] * _loss
+
+            domain_loss = 0
+            loss = segment_loss + domain_loss_weight*domain_loss
+
+            training_metrics(
+                # preds=output_domains.cpu(),
+                # target=domains.cpu(),
+                dice_preds=s_outputs[:, 0, ...].cpu(),
+                dice_target=s_groundtruth[:, 0, ...].type(torch.IntTensor).cpu(),
+                value={
+                    "loss": loss.cpu(),
+                    "segmentation_loss": segment_loss.cpu(),
+                    # "domain_loss": domain_loss.cpu()
+                }
+            )
+
+            # backward
+            loss.backward()
+            optimizer.step()
+
+            # tensorboard logging
+            if (step + 1) % hpconfig.logging_step == 0:
+                values = training_metrics.compute()
+                loader.set_description(
+                    "Training: loss: {:.4f} domain loss: {:.4f} dice: {:.4f}".format(
+                        values["train_Loss"], 0, values["train_Segmentation_Dice"]
+                    )
+                )
+                for v_key in values:
+                    writer.add_scalar(v_key, values[v_key], step)
+
+                writer.add_scalar("alpha", alpha, step)
+                writer.add_scalar("domain_loss_weight", domain_loss_weight, step)
+
+            # Validation
+            if (step + 1) % hpconfig.validation_interval == 0:
+                valid_values = validation(
+                    alb_val_dataloader, None, model, criterion, device, training_weight,
+                    domain_loss_weight, alpha, validation_metrics
+                )
+
+                for v_key in valid_values:
+                    writer.add_scalar(v_key, valid_values[v_key], step)
+
+                save_path = os.path.join(
+                    checkpoint_dir, "%s_checkpoint_%04d.pt" % (name, step)
+                )
+                # store best loss and save a model checkpoint
+                s_best_loss = min(valid_values["validation_Loss"], s_best_loss)
+                torch.save(
+                    {
+                        "step": step,
+                        "epoch": epoch,
+                        "arch": "ResUnet",
+                        "state_dict": model.state_dict(),
+                        "s_best_loss": s_best_loss,
+                        "optimizer": optimizer.state_dict(),
+                    },
+                    save_path,
+                )
+                print("Saved checkpoint to: %s" % save_path)
+
+            step += 1
+
+        domain_loss_weight = min(max_domain_loss_weight, domain_loss_weight * domain_loss_weight_decay)
+        training_metrics.reset()
+
+
+def validation(s_dataloader, t_dataloader, model, criterion, device, training_weight, domain_loss_weight, alpha,
+               validation_metrics: torchmetrics.MetricCollection):
+    print("\nValidation...")
+
+    # s_batch = iter(s_dataloader)
+    # t_batch = iter(t_dataloader)
+
+    #loader_len = min(len(s_dataloader), len(t_dataloader))
+    nllloss = torch.nn.NLLLoss().to(device)
+
+    # switch to evaluate mode
+    model.eval()
+
+    # Iterate over data.
+    for (i, s_data) in enumerate(s_dataloader):
+        # get the inputs and wrap in Variable
+        s_images = s_data['image'].to(device)
+        s_groundtruth = s_data['mask'].to(device)
+        s_domains = torch.zeros(s_images.shape[0]).long().to(device)
+
+        # t_data = next(t_batch)
+        # t_image1 = t_data['x'].to(device)
+        # t_image2 = t_data['y'].to(device)
+        # t_domains = torch.ones(t_image1.shape[0] + t_image2.shape[0]).long().to(device)
+        # domains = torch.cat((s_domains, t_domains), 0)
+
+        s_outputs, s_output_domains = model.segment_forward(s_images, alpha=alpha)
+        # t_output_domains1 = model.domain_classify(x=t_image1, alpha=alpha)
+        # t_output_domains2 = model.domain_classify(x=t_image2, alpha=alpha)
+        # t_output_domains = torch.cat((t_output_domains1, t_output_domains2), 0)
+        # output_domains = torch.cat((s_output_domains, t_output_domains), 0)
+
+        segment_loss = 0.0
+        for i in range(2):
+            _loss = criterion(s_outputs[:, i, ...], s_groundtruth[:, i, ...])
+            segment_loss += training_weight[i] * _loss
+
+        domain_loss = 0
+        loss = segment_loss + domain_loss_weight*domain_loss
+
+        validation_metrics(
+            # preds=output_domains.cpu(),
+            # target=domains.cpu(),
+            dice_preds=s_outputs[:, 0, ...].cpu(),
+            dice_target=s_groundtruth[:, 0, ...].type(torch.IntTensor).cpu(),
+            value={
+                "loss": loss.cpu(),
+                "segmentation_loss": segment_loss.cpu(),
+                # "domain_loss": domain_loss.cpu()
+            }
+        )
+
+    values = validation_metrics.compute()
+    print(
+        "Validation: loss: {:.4f} domain loss: {:.4f} dice: {:.4f}".format(
+            values["validation_Loss"], 0, values["validation_Segmentation_Dice"]
+        )
+    )
+
+    model.train()
+    validation_metrics.reset()
+
+    return values
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Road and Building Extraction")
+    parser.add_argument(
+        "-c", "--config", type=str, required=True, help="yaml file for configuration"
+    )
+    parser.add_argument(
+        "--epochs",
+        default=75,
+        type=int,
+        metavar="N",
+        help="number of total epochs to run",
+    )
+    parser.add_argument(
+        "--resume",
+        default="",
+        type=str,
+        metavar="PATH",
+        help="path to latest checkpoint (default: none)",
+    )
+    parser.add_argument("--name", default="default", type=str, help="Experiment name")
+    parser.add_argument("--mode", default=0, type=int, help="Training mode")
+    parser.add_argument("--device", default="cuda:0", type=str, help="Device ID")
+    args = parser.parse_args()
+
+    hp = HParam(args.config)
+    with open(args.config, "r") as f:
+        hp_str = "".join(f.readlines())
+
+    weights = [1.0, 0.1]
+    device = torch.device(args.device)
+
+    main(hp, num_epochs=args.epochs, resume=args.resume, name=args.name, device=device, training_weight=weights)
